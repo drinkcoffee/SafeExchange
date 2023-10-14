7d96e11ab07b6094f7d8c35a5430928e33a52e7b
Peter Robinson <drinkcoffee@eml.cc>
2023-10-14 10:59:00 +1000

    Create bounty skeleton

--- src/Bounty.sol
+++ src/Bounty.sol
@@ -2,13 +2,40 @@
 pragma solidity ^0.8.19;
 
 contract Bounty {
-    uint256 public number;
+    uint256 public constant BOUNTY = 10 ether;
+    uint256 public constant BOUNTY_PERIOD = 1 days;
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+    address public owner;
+    address public newAdmin;
+    uint256 public start;
+
+    error NotCorrectBounty();
+
+    constructor(address _newAdmin) payable {
+        if (msg.value != BOUNTY) {
+            revert NotCorrectBounty();
+        }
+        owner = msg.sender;
+        newAdmin = _newAdmin;
+        start = block.timestamp;
     }
 
-    function increment() public {
-        number++;
+    // NOTE: transaction must be sent by the account with DEFAULT ADMIN on the business logic contract
+    function claim() external {
+        // TODO call to business logic contract to 
+        // TODO 1. call getRoleMemberCount and check the number of DEFAULT_ADMIN_ROLE is now 1 
+        // TODO 2. grantRole DEFAULT_ADMIN_ROLE for newAdmin
+        // TODO 3. renounceRole DEFAULT_ADMIN_ROLE for msg.sender
+        // Send the BOUNTY to msg.sender
+    }
+
+
+    function refund() external {
+        // TODO check is owner
+        // TODO check if after BOUNTY PERIOD
+        // Transfer $ to owner
+        // TOOD add static analysis override
+        selfdestruct(payable(owner));
+
     }
 }
